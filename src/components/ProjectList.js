e58a02a5fe4596128886ef321e35681ddfe476ae
Essyuge <ugeesther@gmail.com>
2022-11-09 21:09:55 +0300

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,23 +3,24 @@ import { useState, useEffect } from "react";
 import { NavLink, useHistory, useParams, useRouteMatch, useLocation } from "react-router-dom";
 
 const ProjectList = ({
-    projects,
-    onProjectEdit,
-    onProjectDelete,
-    setSelectedCategory,
-    setSearchQuery
-  }) => {
-    
-    const [searchInputText, setSearchInputText] = useState("");
-    const { category } = useParams();
-    // useLocation provides access to query parameters as search
-    const { search } = useLocation();
-    // useHistory gives access to the history object so we can update the url
-    const history = useHistory();
-    // useRouteMatch used to access the url without any query parameters
-    const { url } = useRouteMatch(); 
+  projects,
+  onProjectEdit,
+  onProjectDelete,
+  setSelectedCategory,
+  setSearchQuery
+}) => {
+  
+  const [searchInputText, setSearchInputText] = useState("");
+  const { category } = useParams();
+  // useLocation provides access to query parameters as search
+  const { search } = useLocation();
+  // useHistory gives access to the history object so we can update the url
+  const history = useHistory();
+  // useRouteMatch used to access the url without any query parameters
+  const { url } = useRouteMatch(); 
 
-    console.log('useLocation() search', search);
+  
+  console.log('useLocation() search', search);
   console.log('useRouteMatch url', url);
 
 
@@ -60,6 +61,7 @@ const ProjectList = ({
 
     setSearchInputText(new URLSearchParams(search).get('q'))
   }, [search])
+
 
   return (
     <section>

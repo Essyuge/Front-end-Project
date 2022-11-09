6458a415c5c112bd79c0769fb099a17b51f8233e
Essyuge <ugeesther@gmail.com>
2022-11-09 21:02:38 +0300

    done

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -36,4 +36,81 @@ const initialState = {
           history.push("/projects")
         });
     };
+    return (
+        <section>
+          <form  className='form' autoComplete='off'
+          onSubmit={handleSubmit} >
+          <h3>Add New Book</h3>
+          <label htmlFor="tilte">Title</label>
+           <input 
+           type="text"
+            id="title" 
+            name="title"
+            onChange={handleChange}
+            value={formData.title}
+            />
     
+          <label htmlFor="author">Author</label>
+           <input 
+           type="text" 
+           id="author" 
+           name="author"
+           onChange={handleChange}
+           value={formData.author}
+           />
+           
+    
+          <label htmlFor="summary">Summary</label>
+           <textarea 
+           id="summary" 
+           name="summary"
+           onChange={handleChange}
+           value={formData.summary}
+           ></textarea>
+          
+    
+           <label htmlFor="category">Category</label>
+           <select 
+           name="category" 
+           id="category"
+           onChange={handleChange}
+           value={formData.category}
+           >
+    
+           <option>Select category</option>
+           <option value="Non Fiction">Non Fiction</option>
+           <option value="Comic">Comic</option>
+           <option value="Thiller">Thriller</option>
+           <option value="Romance">Romance</option>
+           <option value="Fiction">Fiction</option>
+           <option value="TextBook">TextBook</option>
+           </select>
+    
+    
+           <lable htmlFor='image'>Image</lable>
+            <input 
+            type="text" 
+            id="image" 
+            name="image"
+            onChange={handleChange}
+            value={formData.image}
+            />
+    
+            <label htmlFor="price">Price</label>
+           <input 
+           type="text" 
+           id="price" 
+           name="price"
+           onChange={handleChange}
+           value={formData.price}
+           
+           />
+           
+    
+            <button type="submit">Add Book</button>
+          </form>
+        </section>
+      );
+    };
+    
+    export default ProjectForm;

362d129caf96f9027569b291d1538734d63d712f
Essyuge <ugeesther@gmail.com>
2022-11-09 21:08:38 +0300

    updates

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,4 +2,21 @@ import ProjectListItem from "./ProjectListItem";
 import { useState, useEffect } from "react";
 import { NavLink, useHistory, useParams, useRouteMatch, useLocation } from "react-router-dom";
 
+const ProjectList = ({
+    projects,
+    onProjectEdit,
+    onProjectDelete,
+    setSelectedCategory,
+    setSearchQuery
+  }) => {
+    
+    const [searchInputText, setSearchInputText] = useState("");
+    const { category } = useParams();
+    // useLocation provides access to query parameters as search
+    const { search } = useLocation();
+    // useHistory gives access to the history object so we can update the url
+    const history = useHistory();
+    // useRouteMatch used to access the url without any query parameters
+    const { url } = useRouteMatch(); 
 
+    

98500e1fb3732db51029a23701b75876c1ead1bf
Essyuge <ugeesther@gmail.com>
2022-11-09 21:04:34 +0300

    updates

--- src/components/ProjectEditForm.js
+++ src/components/ProjectEditForm.js
@@ -11,4 +11,14 @@ const initialState = {
     category:""
     };
 
+    const ProjectEditForm = ({ onUpdateProject }) => {
+        const [formData, setFormData] = useState(initialState);
+      
+        const { title, author, summary, price, image ,category} = formData;
+      
+        const { id } = useParams();
+        const history = useHistory()
+        console.log('id', id)
+
+        
     

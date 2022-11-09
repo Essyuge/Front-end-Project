206e3af89c32abf396dc9c3beeeb5de6afd3a2a7
Essyuge <ugeesther@gmail.com>
2022-11-09 21:05:17 +0300

    updates

--- src/components/ProjectEditForm.js
+++ src/components/ProjectEditForm.js
@@ -20,5 +20,10 @@ const initialState = {
         const history = useHistory()
         console.log('id', id)
 
-        
-    
+        useEffect(() => {
+            fetch(`http://localhost:4000/projects/${id}`)
+              .then((res) => res.json())
+              .then((project) => setFormData(project));
+          }, [id]);
+          
+          

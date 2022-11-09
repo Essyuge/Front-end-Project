1feb7b92ed44a8e14f9fe4f88b818a29c1675d8a
Essyuge <ugeesther@gmail.com>
2022-11-09 20:57:50 +0300

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,2 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+
+const Header = ({ isDarkMode, onToggleDarkMode }) => {
+    const buttonTextContent = isDarkMode ? "Light Mode" : "Dark Mode";
+  
